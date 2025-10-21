5c8d1e825613ca790b72b0aa0bcdc291a90e2b31
Louisa Wu <louisa.wu1@student.uq.edu.au>
2025-10-21 19:03:35 +1000

    Modified and improved the  3D UNet structure to follow the figure in the paper

--- recognition/3d_improved_unet_48027296/modules.py
+++ recognition/3d_improved_unet_48027296/modules.py
@@ -35,41 +35,77 @@ class UNet2D(nn.Module):
     
 
 class UNet3D(nn.Module):
-    """ Standard 3D UNet architecture """
+    """ 
+    3D UNet architecture from: 
+    O. Cicek, A. Abdulkadir, S. S. Lienkamp, T. Brox, and O. Ronneberger, “3D U-Net: Learning 
+    Dense Volumetric Segmentation from Sparse Annotation,” in Medical Image Computing and Computer-
+    Assisted Intervention - MICCAI 2016, ser. Lecture Notes in Computer Science, S. Ourselin, L. 
+    Joskowicz, M. R. Sabuncu, G. Unal, and W. Wells, Eds. Cham: Springer International Publishing, 
+    2016, pp. 424-432.
+    """
     def __init__(self, in_channels=1, out_channels=1, init_features=32):
         super().__init__()
         features = init_features
 
+        # encoder 
         self.encoder1 = self._block(in_channels, features)
         self.pool1 = nn.MaxPool3d(2)
         self.encoder2 = self._block(features, features * 2)
         self.pool2 = nn.MaxPool3d(2)
+        self.encoder3 = self._block(features * 2, features * 4)
+        self.pool3 = nn.MaxPool3d(2)
+        self.encoder4 = self._block(features * 4, features * 8)
+        self.pool4 = nn.MaxPool3d(2)
 
-        self.bottleneck = self._block(features * 2, features * 4)
+        # bottleneck 
+        self.bottleneck = self._block(features * 8, features * 16)
 
+        # decoder 
+        self.up4 = nn.ConvTranspose3d(features * 16, features * 8, kernel_size=2, stride=2)
+        self.decoder4 = self._block(features * 16, features * 8)
+        self.up3 = nn.ConvTranspose3d(features * 8, features * 4, kernel_size=2, stride=2)
+        self.decoder3 = self._block(features * 8, features * 4)
         self.up2 = nn.ConvTranspose3d(features * 4, features * 2, kernel_size=2, stride=2)
         self.decoder2 = self._block(features * 4, features * 2)
         self.up1 = nn.ConvTranspose3d(features * 2, features, kernel_size=2, stride=2)
         self.decoder1 = self._block(features * 2, features)
 
+        # final output 
         self.final = nn.Conv3d(features, out_channels, kernel_size=1)
 
     def _block(self, in_channels, out_channels):
+        """ 
+        Each block: 2 * (3D Convolution --> Batch Normalisation --> ReLU activation function)
+        """
         return nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, 3, padding=1),
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv3d(out_channels, out_channels, 3, padding=1),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
             nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
+        # encoder 
         enc1 = self.encoder1(x)
         enc2 = self.encoder2(self.pool1(enc1))
+        enc3 = self.encoder3(self.pool2(enc2))
+        enc4 = self.encoder4(self.pool3(enc3))
 
-        bottleneck = self.bottleneck(self.pool2(enc2))
+        # bottleneck
+        bottleneck = self.bottleneck(self.pool4(enc4))
         
-        dec2 = self.up2(bottleneck)
+        # decoder with skip connections
+        dec4 = self.up4(bottleneck)
+        dec4 = self.decoder4(torch.cat((dec4, enc4), dim=1))
+        dec3 = self.up3(dec4)
+        dec3 = self.decoder(torch.cat((dec3, enc3), dim=1))
+        dec2 = self.up2(dec3)
         dec2 = self.decoder2(torch.cat((dec2, enc2), dim=1))
         dec1 = self.up1(dec2)
         dec1 = self.decoder1(torch.cat((dec1, enc1), dim=1))
+
+        # final segmentation map 
         return torch.sigmoid(self.final(dec1))
+    
